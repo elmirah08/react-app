2e6cc5b25e7b4eae767985d448b6dd8e6dc2f25b
demanouElmira <elmirademanou@gmail.com>
2022-08-19 12:22:52 +0100

    first release of react app

---
+++ LearningReact/my-react-app/src/App.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {TextField} from '@mui/material';
+import {Box} from '@mui/material';
+import { Button } from '@mui/material';
+
+function App() {
+  
+  return (
+     <Box sx={{height: 300, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <TextField 
+          variant='outlined' 
+          color='secondary' 
+          label='Add a task' 
+          sx={{width: 350}} />
+        <Button variant='contained' color='secondary' sx={{width: 150, height: 56}}>Add Todo</Button> 
+    </Box>
+  )
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
