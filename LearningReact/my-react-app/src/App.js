30452183942dbf7b039fbd742cc08ffc081be179
demanouElmira <elmirademanou@gmail.com>
2022-08-19 12:31:16 +0100

    update App.js with new line

--- LearningReact/my-react-app/src/App.js
+++ LearningReact/my-react-app/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {TextField} from '@mui/material';
+import {Card, TextField} from '@mui/material';
 import {Box} from '@mui/material';
 import { Button } from '@mui/material';
 
@@ -13,6 +13,7 @@ function App() {
           label='Add a task' 
           sx={{width: 350}} />
         <Button variant='contained' color='secondary' sx={{width: 150, height: 56}}>Add Todo</Button> 
+        <Card>hello</Card>
     </Box>
   )
 }
