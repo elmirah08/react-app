d78e346e16fd41fff8cc27da25342e8a0bae97a2
elmirah08 <111579061+elmirah08@users.noreply.github.com>
2022-08-21 15:04:59 +0100

    added another typography element on App.js

--- LearningReact/my-react-app/src/App.js
+++ LearningReact/my-react-app/src/App.js
@@ -24,6 +24,8 @@ function App() {
         </Card>
         <Typography variant='h1'>marah</Typography>
         <Typography variant='h2'>junior</Typography>
+        <Typography variant='h2'>prince</Typography>
+
      </Box>
   )
 }
