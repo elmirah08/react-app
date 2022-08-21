6546764a104e73712a564eada9c34f99028d2362
demanouElmira <elmirademanou@gmail.com>
2022-08-21 19:54:07 +0100

    added a card element to App.js

--- LearningReact/my-react-app/src/App.js
+++ LearningReact/my-react-app/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { TextField} from '@mui/material';
+import { Card, CardContent, TextField, Typography} from '@mui/material';
 import {Box} from '@mui/material';
 import { Button } from '@mui/material';
 
@@ -13,6 +13,11 @@ function App() {
           label='Add a task' 
           sx={{width: 350}} />
         <Button variant='contained' color='secondary' sx={{width: 150, height: 56}}>Add Todo</Button> 
+        <Card>
+          <CardContent>
+            <Typography variant='body'>Lorem ipsum sit dolor amet</Typography>
+          </CardContent>
+        </Card>
      </Box>
   )
 }
