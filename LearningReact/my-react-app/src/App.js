0c86ffff673db0aaa9ea22d7dfdc02fc7ac10aff
demanouElmira <elmirademanou@gmail.com>
2022-08-21 20:38:28 +0100

    fixed an error

--- LearningReact/my-react-app/src/App.js
+++ LearningReact/my-react-app/src/App.js
@@ -4,7 +4,6 @@ import {Box} from '@mui/material';
 import { Button } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
