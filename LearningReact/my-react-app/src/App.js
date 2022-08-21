96d836987e6bf5554dacf35af9946be19d9bb499
demanouElmira <elmirademanou@gmail.com>
2022-08-21 11:21:39 +0100

    added an image to App.js

--- LearningReact/my-react-app/src/App.js
+++ LearningReact/my-react-app/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Card, TextField} from '@mui/material';
+import {Card, CardMedia, TextField, Typography} from '@mui/material';
 import {Box} from '@mui/material';
 import { Button } from '@mui/material';
 
@@ -14,7 +14,15 @@ function App() {
           sx={{width: 350}} />
         <Button variant='contained' color='secondary' sx={{width: 150, height: 56}}>Add Todo</Button> 
         <Card>hello</Card>
-    </Box>
+        <Typography variant='h1'>Elmira</Typography>
+        <Card>
+          <CardMedia
+            component="img"
+            image='LearningReact/my-react-app/src/Okro-Soup.jpeg'
+            alt='food'
+          />
+        </Card>
+     </Box>
   )
 }
 
