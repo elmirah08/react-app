d468a8b379bb57613cfb64356e0f2cc7a76495d2
demanouElmira <elmirademanou@gmail.com>
2022-08-21 20:06:44 +0100

    added App bar element to App.js

--- LearningReact/my-react-app/src/App.js
+++ LearningReact/my-react-app/src/App.js
@@ -2,11 +2,35 @@ import * as React from 'react';
 import { TextField} from '@mui/material';
 import {Box} from '@mui/material';
 import { Button } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   
   return (
      <Box sx={{height: 300, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+        </Box>
           <TextField 
           variant='outlined' 
           color='secondary' 
